e5bafcb632a3f1c6afbe0a58c6c58228da50e4b8
Saeed Soheili <mr.saeed.soheili@gmail.com>
2023-08-13 00:19:26 +0330

    finish part 7

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -5,6 +5,7 @@ import Body3 from "./Body3";
 import Body4 from "./Body4";
 import Body5 from "./Body5";
 import Body6 from "./Body6";
+import Body7 from "./Body7";
 
 export default function Body() {
   return (
@@ -15,6 +16,7 @@ export default function Body() {
       <Body4 />
       <Body5 />
       <Body6 />
+      <Body7 />
     </>
   );
 }
--- src/components/body/Body7.js
+++ src/components/body/Body7.js
@@ -1,6 +1,27 @@
 import React from "react";
 import "./Body7.css";
+import { Col, Row } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import CreditCardIcon from "@mui/icons-material/CreditCard";
+import HeadphonesIcon from "@mui/icons-material/Headphones";
 
 export default function Body7() {
-  return <div>Body7</div>;
+  return (
+    <Row className="row-main-body7">
+      <Col>
+        <p className="title-body7">
+          Unlock Limitless Possibilities with Our New Card Solutions
+        </p>
+        <div className="main-div-body7">
+          <Button className="button1-body7">
+            Unlock your card
+            <CreditCardIcon className="icon1-body7" />
+          </Button>
+          <Button className="button2-body7">
+            Customer support <HeadphonesIcon className="icon2-body7" />
+          </Button>
+        </div>
+      </Col>
+    </Row>
+  );
 }
